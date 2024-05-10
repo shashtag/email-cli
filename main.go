8cd294b416b8ed6fb21ce9538e4309bdf29e61f9
Shashwat Gupta <shashwatsatna@gmail.com>
2024-05-10 23:07:54 +0530

    init cobra

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Shashwat Gupta shashwatsatna@gmail.com
+
+*/
+package main
+
+import "github.com/shashtag/multiple-csv-to-outlook-mail-mvp/cmd"
+
+func main() {
+	cmd.Execute()
+}
