b8c16e5a8fc757156b6b3d40729ccbf927375c7c
Shashwat Gupta <shashwatsatna@gmail.com>
2024-05-13 21:27:21 +0530

    ✨ logic for html mail

--- internal/createEmail/sendEmail.go
+++ internal/createEmail/sendEmail.go
@@ -10,8 +10,10 @@ import (
 )
 
 func SendEmail() error {
+	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
+	msg := "Subject:" + "wsgm" + "\n" + headers + "\n\n" + "<div>Hi, This is a test email</div>"
 	auth := smtp.PlainAuth("", viper.GetString("Email"), viper.GetString("Password"), viper.GetString("SMTP_SERVER"))
-	err := smtp.SendMail(viper.GetString("SMTP_SERVER")+":587", auth, "shashwatsatna@gmail.com", []string{"shashwatsatna@gmail.com"}, []byte("Hello, World!"))
+	err := smtp.SendMail(viper.GetString("SMTP_SERVER")+":587", auth, "shashwatsatna@gmail.com", []string{"shashwatsatna@gmail.com"}, []byte(msg))
 	if err != nil {
 		log.Fatal(err)
 		return err
